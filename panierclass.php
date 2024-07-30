1a811e46ac6861de94057e48941682e0821222fa
Hitami860 <Hitami86000@gmail.com>
2024-07-30 13:24:47 +0200

    upd

---
+++ panierclass.php
@@ -0,0 +1,27 @@
+<?php
+
+class panier {
+
+    
+
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Panier</title>
+</head>
+<body>
+    
+
+
+</body>
+</html>
