dcde2f3b8b3d8e0a3c8a7774d98ebe7fb013aa96
Hitami860 <Hitami86000@gmail.com>
2024-08-01 16:44:23 +0200

    upd

--- classe/panierclass.php
+++ classe/panierclass.php
@@ -1,6 +1,6 @@
 <?php
 require_once("./config/db.php");
-class panier extends db 
+class panier extends db
 {
 
     private $produits_id;
@@ -18,123 +18,112 @@ class panier extends db
     private $calculetotal;
 
 
-public function __construct()
+    public function __construct()
     {
         $this->db = $this->connecte();
     }
 
-public function setProduitsid($produits_id)
+    public function setProduitsid($produits_id)
 
-{
+    {
 
-$this->produits_id=$produits_id;
-
-}
-
-public function getProduitsid()
-
-{
-
-return $this->produits_id;
-
-}
-
-public function setTotal($total)
-
-{
-
-$this->total=$total;
-
-}
-
-public function getTotal()
-
-{
-
-    return $this->total;
-
-}
-public function setRowguid($rowguid)
-
-{
-
-$this->rowguid=$rowguid;
-
-}
-
-public function getRowguid()
-
-{
-
-    return $this->rowguid;
-
-}
-
-public function setReductionid($reduction_id)
-
-{
-
-    $this->reduction_id=$reduction_id;
-
-}
-
-public function getReductionid()
-
-{
-
-    return $this->reduction_id;
-
-}
-
-
-
-public function getArticle($id){
-    $sql = $this->db->prepare("SELECT * FROM produits WHERE id = :id");
-    $sql->bindParam(":id", $id);
-    $sql->execute();
-    return $sql->fetch(PDO::FETCH_ASSOC);
-}
-
-
-
-
-public function addPanier()
-{
-
- 
-    $produits_id = $this->getProduitsid();
-    $total = $this->getTotal();
-    $reduction_id = $this->getReductionid();
-    $rowguid = $this->getRowguid();
-
-    $sql2 = $this->db->prepare("SELECT * FROM panier WHERE rowguid = :rowguid AND produits_id = :produits_id");
-    $sql2->bindParam(":produits_id", $produits_id);
-    $sql2->bindParam(':rowguid', $rowguid);
-    $sql2->execute();
-    $exist = $sql2->fetch(PDO::FETCH_ASSOC);
-    var_dump($exist);
-    if($exist){
-        $sql = $this->db->prepare("UPDATE panier SET total = :total WHERE rowguid = :rowguid AND produits_id = :produits_id");
-        $sql->bindParam(':produits_id', $exist["produits_id"]);
-        $sql->bindParam(':rowguid', $rowguid);
-        $sql->bindParam(':total', $total);
-    }else{
-        $sql = $this->db->prepare("INSERT INTO panier (produits_id, total, reduction_id, rowguid) VALUES (:produits_id, :total, :reduction_id, :rowguid)");
-        $sql->bindParam(':produits_id', $produits_id);
-        $sql->bindParam(':rowguid', $rowguid);
-        $sql->bindParam(':total', $total);
-        $sql->bindParam(':reduction_id', $reduction_id);
+        $this->produits_id = $produits_id;
     }
 
- 
-    $sql->execute();
+    public function getProduitsid()
 
-}
+    {
 
-public function calculerTotal()
-{
-    
-}
+        return $this->produits_id;
+    }
+
+    public function setTotal($total)
+
+    {
+
+        $this->total = $total;
+    }
+
+    public function getTotal()
+
+    {
+
+        return $this->total;
+    }
+    public function setRowguid($rowguid)
+
+    {
+
+        $this->rowguid = $rowguid;
+    }
+
+    public function getRowguid()
+
+    {
+
+        return $this->rowguid;
+    }
+
+    public function setReductionid($reduction_id)
+
+    {
+
+        $this->reduction_id = $reduction_id;
+    }
+
+    public function getReductionid()
+
+    {
+
+        return $this->reduction_id;
+    }
 
 
+
+    public function getArticle($id)
+    {
+        $sql = $this->db->prepare("SELECT * FROM produits WHERE id = :id");
+        $sql->bindParam(":id", $id);
+        $sql->execute();
+        return $sql->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+
+
+    public function addPanier()
+    {
+
+
+        $produits_id = $this->getProduitsid();
+        $total = $this->getTotal();
+        $reduction_id = $this->getReductionid();
+        $rowguid = $this->getRowguid();
+
+        $sql2 = $this->db->prepare("SELECT * FROM panier WHERE rowguid = :rowguid AND produits_id = :produits_id");
+        $sql2->bindParam(":produits_id", $produits_id);
+        $sql2->bindParam(':rowguid', $rowguid);
+        $sql2->execute();
+        $exist = $sql2->fetch(PDO::FETCH_ASSOC);
+        var_dump($exist);
+        if ($exist) {
+            $sql = $this->db->prepare("UPDATE panier SET total = :total WHERE rowguid = :rowguid AND produits_id = :produits_id");
+            $sql->bindParam(':produits_id', $exist["produits_id"]);
+            $sql->bindParam(':rowguid', $rowguid);
+            $sql->bindParam(':total', $total);
+        } else {
+            $sql = $this->db->prepare("INSERT INTO panier (produits_id, total, reduction_id, rowguid) VALUES (:produits_id, :total, :reduction_id, :rowguid)");
+            $sql->bindParam(':produits_id', $produits_id);
+            $sql->bindParam(':rowguid', $rowguid);
+            $sql->bindParam(':total', $total);
+            $sql->bindParam(':reduction_id', $reduction_id);
+        }
+
+
+        $sql->execute();
+    }
+
+    public function calculerTotal()
+    {
+    }
 }
--- panier.php
+++ panier.php
@@ -11,7 +11,7 @@ $articles = $_SESSION['panier'];
 var_dump($articles);
 
 
-$total=0;
+$total = 0;
 
 
 ?>
@@ -37,60 +37,60 @@ $total=0;
     <section>
         <div class="container mx-auto flex flex-col gap-6">
 
-        <h2 class="text-bold flex justify-center items-center uppercase font-bold text-orange-400 pb-6 pt-6">Mon panier</h2>
+            <h2 class="text-bold flex justify-center items-center uppercase font-bold text-orange-400 pb-6 pt-6">Mon panier</h2>
 
-        <table class="border border-2 border-black mt-4">
+            <table class="border border-2 border-black mt-4">
 
-            <thead>
+                <thead>
 
-                <th></th>
-                <th class="border border border-2 border-black">Produit</th>
-                <th class="border border border-2 border-black">Prix</th>
-                <th class="border border border-2 border-black">Quantite</th>
-                <th class="border border border-2 border-black">Action</th>
-                <th class="border border border-2 border-black" >Total</th>
+                    <th></th>
+                    <th class="border border border-2 border-black">Produit</th>
+                    <th class="border border border-2 border-black">Prix</th>
+                    <th class="border border border-2 border-black">Quantite</th>
+                    <th class="border border border-2 border-black">Action</th>
+                    <th class="border border border-2 border-black">Total</th>
 
-            </thead>
-            <tbody>
-                <?php
-                foreach ($articles as $key => $article) {
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($articles as $key => $article) {
 
-          
-                   /* var_dump($article); */
-                    $produit = $newPanier->getArticle($key);
-                    /* var_dump($produit); */
-                    $total += ($produit['prix'] * $article);
-                ?>
 
+                        /* var_dump($article); */
+                        $produit = $newPanier->getArticle($key);
+                        /* var_dump($produit); */
+                        $total += ($produit['prix'] * $article);
+                    ?>
+
+                        <tr>
+                            <td class="border border-2 border-black"><img class="h-[7%] w-[7%]" src="<?php echo $produit['image'] ?>" alt="Image du produit"></td>
+                            <td class="border border-2 border-black"><?php echo $produit['name'] ?></td>
+                            <td class="border border-2 border-black"><?php echo $produit['prix'] ?></td>
+                            <td class="border border-2 border-black"><?= $article ?></td>
+                            <td class="border border-2 border-black"><a href="delPanier.php?id=<?php print $produit["id"] ?>"><img class="w-[7%] h-[7%]" src="iconepoubelle.webp" alt="icone poubelle"></a></td>
+
+                        </tr>
+                    <?php
+                    } ?>
+                </tbody>
+                <tfoot>
                     <tr>
-                        <td class="border border-2 border-black"><img class="h-[7%] w-[7%]" src="<?php echo $produit['image'] ?>" alt="Image du produit"></td>
-                        <td class="border border-2 border-black"><?php echo $produit['name'] ?></td>
-                        <td class="border border-2 border-black"><?php echo $produit['prix'] ?></td>
-                        <td class="border border-2 border-black"><?= $article ?></td>
-                        <td class="border border-2 border-black"><a href="delPanier.php?id=<?php print $produit["id"] ?>"><img class="w-[7%] h-[7%]" src="iconepoubelle.webp" alt="icone poubelle"></a></td>
-
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><?php print $total ?></td>
                     </tr>
-                <?php
-                } ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><?php print $total ?></td>
-                </tr>
-            </tfoot>
-        </table>
+                </tfoot>
+            </table>
 
-        <table>
-        </table>
+            <table>
+            </table>
 
-                <div class="flex justify-center">
-        <button class="border border-orange-400 bg-orange-400 w-[10%] flex justify-center items-center" type="submit" name="acheter">Acheter</button>
-        </div>
+            <div class="flex justify-center">
+                <button class="border border-orange-400 bg-orange-400 w-[10%] flex justify-center items-center font-bold" type="submit" name="acheter">Acheter</button>
+            </div>
 
         </div>
 
