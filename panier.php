0cd018f78787b501ffa610be7ea4c896bc1ccb9f
said-ctrl <fifinabs@gmail.com>
2024-08-06 14:38:38 +0200

    ude

--- panier.php
+++ panier.php
@@ -102,6 +102,7 @@ $total = 0;
                         <div class="flex justify-between items-center">
                             <div class="flex items-center gap-4">
                                 <form action="" method="post">
+                                  
                                     <button type="submit" name="moins" value="<?php print $produit["id"] ?>" class="group rounded-[50px] border border-gray-200 shadow-sm shadow-transparent p-2.5 flex items-center justify-center bg-white transition-all duration-500 hover:shadow-gray-200 hover:bg-gray-50 hover:border-gray-300 focus-within:outline-gray-300">
                                         <svg class="stroke-gray-900 transition-all duration-500 group-hover:stroke-black" width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M4.5 9.5H13.5" stroke="" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
